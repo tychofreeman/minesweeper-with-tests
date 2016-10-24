8b295abfa272bd1b3b1b839c78fe87bcabd51a8d
Christopher Freeman <cwfreeman@gmail.com>
2016-10-23 23:17:06 -0500

    Added new test; fixed Assert. statements. Tried Approvals, but it's the wrong library.

--- src/test/java/com/cwfreeman/refactoring/MinesweeperTest.java
+++ src/test/java/com/cwfreeman/refactoring/MinesweeperTest.java
@@ -14,6 +14,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.*;
+
 public class MinesweeperTest
 {
     Minesweeper minesweeper;
@@ -70,14 +72,13 @@ public class MinesweeperTest
         minesweeper.init();
 
         for (int i = 0; i < 81; i++) {
-            minesweeper.adjacent[i] = 0;
+            minesweeper.adjacent[i] = Minesweeper.unexposed;
         }
         minesweeper.adjacent[0] = Minesweeper.mine;
         minesweeper.adjacent[1] = 1;
+        minesweeper.adjacent[9] = 1;
         minesweeper.adjacent[10] = 1;
-        minesweeper.adjacent[11] = 1;
         before = minesweeper.exposed.clone();
-
     }
 
     @After
@@ -88,38 +89,48 @@ public class MinesweeperTest
 
     @Test
     public void noExposedMinesOnInit() {
-        Assert.assertNotNull(minesweeper.exposed);
-        Assert.assertEquals(true, Arrays.stream(minesweeper.exposed).noneMatch((x) -> x != -4));
+        assertNotNull(minesweeper.exposed);
+        assertEquals(true, Arrays.stream(minesweeper.exposed).noneMatch((x) -> x != -4));
     }
 
     @Test
     public void clicksOnMineEndsGame() {
         minesweeper.adjacent[0] = Minesweeper.mine;
         minesweeper.expose(0, 0);
-        Assert.assertEquals(Minesweeper.sad, minesweeper.sadness);
-    }
-
-    @Test
-    public void clicksOnSquareAdjacentToMine() {
-        minesweeper.expose(8,8);
-
-        Assert.assertEquals(Minesweeper.bored, minesweeper.sadness);
-        for (int i = 0; i < before.length; i++) {
-            if (i > 1 && i != 9 && i != 10) {
-                Assert.assertTrue(String.format("At index %d - expected %d and got %d", i, before[i], minesweeper.exposed[i]),
-                        minesweeper.exposed[i] >= Minesweeper.listEnd);
-            }
-        }
-        Assert.assertEquals(0, minesweeper.exposed[0]);
-        Assert.assertEquals(0, minesweeper.exposed[1]);
-        Assert.assertEquals(0, minesweeper.exposed[9]);
-        Assert.assertEquals(0, minesweeper.exposed[10]);
-
+        assertEquals(Minesweeper.sad, minesweeper.sadness);
     }
 
     @Test
     public void clicksOnEmptySquare() {
+        minesweeper.expose(8,8);
 
+        assertEquals(Minesweeper.bored, minesweeper.sadness);
+        for (int i = 0; i < before.length; i++) {
+            if (i != 1 && i != 9 && i != 10) {
+                assertTrue(String.format("At index %d - expected %d >= %d", i, minesweeper.exposed[i], Minesweeper.unexposed),
+                            minesweeper.exposed[i] >= Minesweeper.unexposed);
+            }
+        }
+        assertEquals(Minesweeper.unexposed, minesweeper.exposed[0]);
+        assertEquals(Minesweeper.unexposed, minesweeper.exposed[1]);
+        assertEquals(Minesweeper.unexposed, minesweeper.exposed[9]);
+        assertEquals(Minesweeper.unexposed, minesweeper.exposed[10]);
+    }
+
+    @Test
+    public void clicksOnSquareAdjacentToMine() {
+        minesweeper.expose(0, 1);
+
+        assertEquals(Minesweeper.bored, minesweeper.sadness);
+        for (int i = 0; i < before.length; i++) {
+            if (i != 0 && i != 1 && i != 9 && i != 10) {
+                assertEquals(String.format("At index %d - expected %d = %d", i, before[i], minesweeper.exposed[i]),
+                        before[i], minesweeper.exposed[i]);
+            }
+        }
+        assertEquals(Minesweeper.unexposed, minesweeper.exposed[0]);
+        assertTrue(minesweeper.exposed[9] == Minesweeper.unexposed ||  minesweeper.exposed[9] == Minesweeper.listEnd);
+        assertEquals(Minesweeper.unexposed, minesweeper.exposed[10]);
     }
 
 
